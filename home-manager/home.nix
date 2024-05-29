aea0c3ae7ad2b0f94bc9da4bb139c5965631352d
Nikita Kuzin <nkuzin@saber.games>
2024-05-30 02:03:43 +0300

    fix hm modules

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,7 +14,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    nix-colors.homeManagerModules.default
+    inputs.nix-colors.homeManagerModules.default
     ./features/hyprland.nix
   ];
 
