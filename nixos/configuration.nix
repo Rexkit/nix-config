eb8c4ce46c52f1cbf0dc078704c0e5689a880314
Nikita Kuzin <nkuzin@saber.games>
2024-05-30 03:39:50 +0300

    fix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -95,19 +95,15 @@
     enable = true;
     layout = "us";
     xkbVariant = "";
-
-    displayManager = {
-      sddm = {
-        enable = true;
-        wayland.enable = true;
-        theme = "where_is_my_sddm_theme";
-      };
-    };
   };
 
-  environment.systemPackages = with pkgs; [
-    where-is-my-sddm-theme
-  ];
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+      theme = "breeze";
+    };
+  };
 
   users.users = {
     nkuzin = {
