f28e07192651fd7c74151d6e5e10ec700ed9e32a
Nikita Kuzin <nkuzin@saber.games>
2024-05-31 11:39:18 +0300

    fix

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -155,6 +155,8 @@
 	};
 
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
 
   sound.enable = true;
   hardware.pulseaudio.enable = false;
