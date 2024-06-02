6a78ba0393d4ccb85c496d2970c740428e831cd0
Nikita Kuzin <nkuzin@saber.games>
2024-06-03 01:16:45 +0300

    fix

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -213,6 +213,10 @@
     git
   ];
 
+  environment.sessionVariables = {
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
