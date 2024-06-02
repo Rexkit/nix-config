39451c069ff46f8ac0092f9682faf0781ed3a7b8
Nikita Kuzin <nkuzin@saber.games>
2024-06-02 03:04:39 +0300

    fix

--- home/default.nix
+++ home/default.nix
@@ -16,6 +16,7 @@
     # ./nvim.nix
     inputs.catppuccin.homeManagerModules.catppuccin
     inputs.nix-colors.homeManagerModules.default
+    inputs.hyprland.homeManagerModules.default
     ../nixos/common/deploy_dots.nix
     ./features/fonts
     ./features/alacritty
