6e6691415e7a41a36b5e6e78c6d79daf5b714cd4
Nikita Kuzin <nkuzin@saber.games>
2024-06-02 23:03:11 +0300

    sddm

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -104,6 +104,7 @@
     displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      theme = "catppuccin-mocha";
     };
   };
 
@@ -203,6 +204,12 @@
     neofetch
     btop
     git
+
+    pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      font  = "Noto Sans";
+      fontSize = "9";
+    }
   ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
