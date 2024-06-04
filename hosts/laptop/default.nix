30455d03d42bfb703073f84dee9f1c3a00fbf5ac
Nikita Kuzin <nkuzin@saber.games>
2024-06-04 03:33:59 +0300

    try

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -77,6 +77,7 @@
   # Enable networking
   networking.networkmanager.enable = true;
   networking.networkmanager.dns = "systemd-resolved";
+  services.resolved.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Moscow";
